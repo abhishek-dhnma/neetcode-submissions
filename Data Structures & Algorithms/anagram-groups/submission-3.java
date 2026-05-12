29fe2862ab899408561f829dacf9383d5df3d6db
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2026-05-12 23:02:50 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        //List<List<String>> ans = new ArrayList<>();
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String s : strs){
+
+            char[] arr = s.toCharArray();
+
+            Arrays.sort(arr);
+
+            String key = new String(arr);
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+
+        }
+
+        
+        // for(Map.Entry<String, List<String>> entry : map.entrySet() ){
+        //     List<String> templist = entry.getValue();
+        //     ans.add(templist);
+        // }
+
+        // for(List<String> value : map.values()){
+        //     ans.add(value);
+        // }
+        
+
+
+        // n * k log k + n 
+
+
+        //return ans;
+        return new ArrayList<>(map.values());
+        
+    }
+}
