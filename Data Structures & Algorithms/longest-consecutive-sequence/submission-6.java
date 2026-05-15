5f1a92775e54161e744ee7b4ad2b248ca8950237
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2026-05-15 16:38:33 +0530

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.java
@@ -0,0 +1,37 @@
+    class Solution {
+        public int longestConsecutive(int[] nums) {
+
+            // Brute Force 
+            // Sorting - O(n log n) + iterate O(N) ~ O(n log n)
+
+           
+           
+            int n = nums.length;
+            if(n == 0) return 0;
+
+             int globalans = 0;
+             Arrays.sort(nums);
+            int i = 0;
+            int currcount = 1;
+            while(i < n){
+            
+                if(i < n-1 && nums[i] + 1 == nums[i+1]){
+                    currcount++;
+                    
+                }else if(i < n-1 && nums[i] == nums[i+1]){
+                    
+                }
+                else {
+                    currcount = 1;
+                }
+
+                globalans = Math.max(currcount, globalans);
+
+                i++;
+                
+            }
+
+            return globalans;
+
+        }
+    }
