52bd9df088ddecb606ece940e1ecbd1a27eaacd8
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2026-06-16 01:19:10 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        // The Core Concept: Using Corners as Decision Tree RootsFor this algorithm to work,
+        // the matrix must be sorted in two specific ways:Each row is sorted from left to right.Each column is sorted from top to bottom.
+        //When you stand at the top-right element $(row = 0, col = matrix[0].length - 1)$, you are at a very unique strategic position. 
+        //It acts like the root of a Binary Search Tree:
+        //Everything to its left in the same row is smaller.Everything below it in the same column is greater.
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int row = 0;
+        int col = m-1;
+
+        while(col >=0  && row <n ){
+            
+            if(target == matrix[row][col] ){
+                return true;
+            }else if(target > matrix[row][col]){
+                row++;
+            }else{
+                col--;
+            }
+
+        }
+
+        return false;
+        
+    }
+}
